3ac649eef6e34ed99ddf143be14c583ebfa87758
KevZho <KevZho141@gmail.com>
2013-10-11 19:58:29 -0700

    Add popup add word function

--- popup.js
+++ popup.js
@@ -4,4 +4,7 @@ window.onload = function() {
 			chrome.tabs.sendMessage(tab.id, "reinit", null);
 		});
 	}, false);
+	document.getElementById("add").addEventListener("click", function() {
+		document.getElementById("status").innerHTML = autoCorrectChromeExtension.addAutoCompleteWord(document.getElementById("incorrect").value, document.getElementById("correct").value);
+	}, false);
 }
