ea38d39cd19961c9726691cf0fd802a0fbde9d9d
KevZho <KevZho141@gmail.com>
2013-10-11 23:25:24 -0700

    make options a button

--- popup.js
+++ popup.js
@@ -7,4 +7,7 @@ window.onload = function() {
 	document.getElementById("add").addEventListener("click", function() {
 		document.getElementById("status").innerHTML = autoCorrectChromeExtension.addAutoCompleteWord(document.getElementById("incorrect").value, document.getElementById("correct").value);
 	}, false);
+	document.getElementById("openOptions").addEventListener("click", function() {
+		location = "options.html";
+	}, false);
 };
